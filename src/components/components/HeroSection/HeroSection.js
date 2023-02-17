74007d4daa95c00a195f69a0b98294e1b215ebde
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-02-17 22:33:58 +0100

    corrected classed and some styling issues

--- src/components/components/HeroSection/HeroSection.js
+++ src/components/components/HeroSection/HeroSection.js
@@ -4,18 +4,17 @@ import "./HeroSection.scss";
 function HeroSection() {
 	return (
 		<div className="HeroSection">
-			<div className="HeroSection-main">
-				<h1 className="HeroSection-main__title">
-					Uniting{" "}
-					<span className="HeroSection-main__title--circled">skill</span> with
-					opportunity
+			<div className="HeroSection__main">
+				<h1 className="HeroSection__title">
+					Uniting <span className="HeroSection__title--circled">skill</span>{" "}
+					with opportunity
 				</h1>
-				<p className="HeroSection-main__text">
+				<p className="HeroSection__text">
 					SkillChain connects job seekers with top employers to find the perfect
 					match for job vacancies.
 				</p>
 				<a href="#" target="_blank">
-					<button className="HeroSection-main__button">View Jobs</button>
+					<button className="HeroSection__button">View Jobs</button>
 				</a>
 			</div>
 			<div className="HeroSection__image"></div>
