c0bee699881f2c9b081fa5c89d5d70f0ff72a36a
Anna Lepak <anna.lepak@adesso.de>
2023-02-17 17:27:12 +0100

    changed BEM names

--- src/components/container/ReasonCard/ReasonCard.js
+++ src/components/container/ReasonCard/ReasonCard.js
@@ -5,9 +5,8 @@ function ReasonCard({ icon, title, description }) {
   return (
     <div className="ReasonCard">
       <div className="ReasonCard__icon">
-        <div className="ReasonCard__icon--background">
-          <img className="ReasonCard__icon--image" src={icon.path} alt={icon.description} />
-        </div>
+        <div className="ReasonCard__icon__background"></div>
+          <img className="ReasonCard__icon__background__image" src={icon.path} alt={icon.description} />
       </div>
       <div className="ReasonCard__title">{title}</div>
       <div className="ReasonCard__description">{description}</div>
