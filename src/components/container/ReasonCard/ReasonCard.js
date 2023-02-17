76c46411405fcbf195de874c17b3c7a46d5c70d6
Anna Lepak <anna.lepak@adesso.de>
2023-02-17 17:12:29 +0100

    removed global styles, moved selectors to ReasonCard selector and added alt property to img

--- src/components/container/ReasonCard/ReasonCard.js
+++ src/components/container/ReasonCard/ReasonCard.js
@@ -5,11 +5,11 @@ function ReasonCard({ icon, title, description }) {
   return (
     <div className="ReasonCard">
       <div className="ReasonCard__icon">
-        <div className="icon__background">
-          <img src={icon} />
+        <div className="ReasonCard__icon--background">
+          <img className="ReasonCard__icon--image" src={icon.path} alt={icon.description} />
         </div>
       </div>
-      <div className="ReasonCard_title">{title}</div>
+      <div className="ReasonCard__title">{title}</div>
       <div className="ReasonCard__description">{description}</div>
     </div>
   );
