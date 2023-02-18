c3b9c266560dc6985b07a431f56ae13bbc950fae
Raveen Liyanage <raveenlng@gmail.com>
2023-02-18 23:15:50 +0530

    Created Mobile Navigation component

--- src/components/container/App/App.js
+++ src/components/container/App/App.js
@@ -1,8 +1,10 @@
+import MobileNavigation from '../../MobileNavigation/MobileNavigation';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <MobileNavigation />
     </div>
   );
 }
