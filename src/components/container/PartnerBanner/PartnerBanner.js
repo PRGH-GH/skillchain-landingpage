dbb50c4ae3eead9d33613fe5be938f39536dcdf8
Amindu <amindu1999@gmail.com>
2023-02-18 11:41:32 +0530

    added slider

---
+++ src/components/container/PartnerBanner/PartnerBanner.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './PartnerBanner.scss'
+import Logo from './../../../resources/prgh-dark-main.svg';
+//this method gonna leads to so many imports so this is the right method?
+
+function PartnerBanner() {
+  const BANNERS = Array(10).fill({ name: "PRGH", src: Logo })  //dev version to add multiple logos
+
+  return (
+    <div className='PartnerBanner'>
+      <div>
+        <img alt="start" />
+        {BANNERS.map((b, i) => <img key={i} src={b.src} alt={b.name} />)}
+        <img alt="end" />
+      </div>
+    </div>
+  )
+}
+
+export default PartnerBanner
