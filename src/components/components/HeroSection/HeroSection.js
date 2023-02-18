5a73a9b4192727e884994efa3634102743c6db96
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-02-18 16:56:03 +0100

    Corrected styles.css code to make it responsive

--- src/components/components/HeroSection/HeroSection.js
+++ src/components/components/HeroSection/HeroSection.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./HeroSection.scss";
+import HeroImage from "./img/hero-image.svg";
 
 function HeroSection() {
 	return (
 		<div className="HeroSection">
-			<div className="HeroSection__main">
+			<div className="HeroSection__info">
 				<h1 className="HeroSection__title">
 					Uniting <span className="HeroSection__title--circled">skill</span>{" "}
 					with opportunity
@@ -13,11 +14,20 @@ function HeroSection() {
 					SkillChain connects job seekers with top employers to find the perfect
 					match for job vacancies.
 				</p>
-				<a href="#" target="_blank">
+				<a
+					href="https://www.google.de/"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
 					<button className="HeroSection__button">View Jobs</button>
 				</a>
 			</div>
-			<div className="HeroSection__image"></div>
+			<div className="HeroSection__image">
+				<img
+					src={HeroImage}
+					alt="Smiling Candidate surrounded by programming languages tags"
+				/>
+			</div>
 		</div>
 	);
 }
