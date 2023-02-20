4ae7c4fb626ff52cbf2df711f0b8fa61e222a743
Amindu <amindu1999@gmail.com>
2023-02-20 15:33:33 +0530

    removed comments

--- src/components/container/PartnerBanner/PartnerBanner.js
+++ src/components/container/PartnerBanner/PartnerBanner.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './PartnerBanner.scss';
 import Logo from './assets/partnerLogos/prgh-dark-main.svg';
-//this method gonna leads to so many imports so this is the right method?
 
 function PartnerBanner() {
-  const BANNERS = Array(10).fill({ name: "PRGH", src: Logo }); //dev version to add multiple logos
+  const banners = Array(10).fill({ name: "PRGH", src: Logo });
 
   return (
     <div className='PartnerBanner'>
       <div>
         <img alt="start" />
-        {BANNERS.map((b, i) => <img key={i} src={b.src} alt={b.name} />)}
+        {banners.map((b, i) => <img key={i} src={b.src} alt={b.name} />)}
         <img alt="end" />
       </div>
     </div>
