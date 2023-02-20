a2482980b3ed6ba02fc5ee18f8fafd9c3210cf84
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-02-20 16:06:23 +0100

    reverted App.js to the original state, yet again))

--- src/components/container/App/App.js
+++ src/components/container/App/App.js
@@ -1,10 +1,8 @@
 import './App.scss';
-import HeroSection from "../../components/HeroSection/HeroSection";
 
 function App() {
   return (
     <div className="App">
-      <HeroSection />
     </div>
   );
 }
