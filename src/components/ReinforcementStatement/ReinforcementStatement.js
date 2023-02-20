8216519876444e8c7e584ee26cb04305b605dd94
Amindu <amindu1999@gmail.com>
2023-02-20 16:05:16 +0530

    refactored to  Reinforcement statement

--- src/components/ReinforcementStatement/ReinforcementStatement.js
+++ src/components/ReinforcementStatement/ReinforcementStatement.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import CallToActionImage from './assets/CallToActionImage.jpg';
-import './CallToAction.scss';
+import ReinforcementStatementImage from './assets/ReinforcementStatementImage.jpg';
+import './ReinforcementStatement.scss';
 
 
-function CallToAction() {
+function ReinforcementStatement() {
     return (
-        <div className='CallToAction'>
-            <img src={CallToActionImage} alt='CallToActionImage' />
-            <div className='CallToAction__text'>
+        <div className='ReinforcementStatement'>
+            <img src={ReinforcementStatementImage} alt='ReinforcementStatement' />
+            <div className='ReinforcementStatement__text'>
                 <h2>Unleash your potential with SkillChain</h2>
-                <div className='CallToAction__underLine' />
+                <div className='ReinforcementStatement__underLine' />
                 <p>
                     SkillChain is a modern job search platform that connects job seekers with top employers to find the perfect match for job vacancies. It uses the latest technologies, including AI, to offer a personalized and efficient job search experience. SkillChain also provides continuous upskilling opportunities to help job seekers advance their careers and remain competitive in the job market. With SkillChain, job seekers can expect a smarter, faster, and more effective way to find their next career opportunity.
                 </p>
@@ -18,4 +18,4 @@ function CallToAction() {
     );
 }
 
-export default CallToAction;
+export default ReinforcementStatement;
