44bbbc5dbb36f2bb491a8078bcdaae9c48d1f7bd
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-02-20 15:12:34 +0100

    removed components and files  from another ticket

--- src/components/components/CandidateProfileCard/CandidateProfileCard.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./CandidateProfileCard";
-
-function CandidateProfileCard() {
-  return (
-    <div>CandidateProfileCard</div>);
-}
-
-export default CandidateProfileCard;
