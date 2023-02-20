6bea2638d3b975c4c22e6f389ade937b1f1129e9
Amindu <amindu1999@gmail.com>
2023-02-20 22:28:09 +0530

    renamed asset and break the lines

--- src/components/ReinforcementStatement/ReinforcementStatement.js
+++ src/components/ReinforcementStatement/ReinforcementStatement.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReinforcementStatementImage from './assets/ReinforcementStatementImage.jpg';
+import reinforcementStatementImage from './assets/reinforcementStatementImage.jpg';
 import './ReinforcementStatement.scss';
 
 
 function ReinforcementStatement() {
     return (
         <div className='ReinforcementStatement'>
-            <img src={ReinforcementStatementImage} alt='ReinforcementStatement' />
+            <img src={reinforcementStatementImage} alt='ReinforcementStatement' />
             <div className='ReinforcementStatement__text'>
                 <h2>Unleash your potential with SkillChain</h2>
                 <div className='ReinforcementStatement__underLine' />
                 <p>
-                    SkillChain is a modern job search platform that connects job seekers with top employers to find the perfect match for job vacancies. It uses the latest technologies, including AI, to offer a personalized and efficient job search experience. SkillChain also provides continuous upskilling opportunities to help job seekers advance their careers and remain competitive in the job market. With SkillChain, job seekers can expect a smarter, faster, and more effective way to find their next career opportunity.
+                    SkillChain is a modern job search platform that connects job seekers with top employers to find the perfect match for job vacancies.
+                    It uses the latest technologies, including AI, to offer a personalized and efficient job search experience.
+                    SkillChain also provides continuous upskilling opportunities to help job seekers advance their careers and remain competitive in the job market.
+                    With SkillChain, job seekers can expect a smarter, faster, and more effective way to find their next career opportunity.
                 </p>
             </div>
         </div>
