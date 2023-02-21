c1a4a9a0e0b30cf85d0f325384d865677019f1dd
Amindu <amindu1999@gmail.com>
2023-02-21 23:43:57 +0530

    Fixed some animations

--- src/components/container/PartnerBanner/PartnerBanner.js
+++ src/components/container/PartnerBanner/PartnerBanner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './PartnerBanner.scss';
-import Logo from './assets/partnerLogos/prgh-dark-main.svg';
+import Logo from './assets/partnerLogos/prghDarkMain.svg';
 
 function PartnerBanner() {
   const banners = Array(10).fill({ name: "PRGH", src: Logo });
@@ -11,7 +11,7 @@ function PartnerBanner() {
         {banners.map((b, i) => <img key={i} src={b.src} alt={b.name} />)}
       </div>
     </div>
-  )
+  );
 }
 
 export default PartnerBanner;
