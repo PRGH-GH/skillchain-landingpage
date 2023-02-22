6d1d041d2eed9b804785da28f06a53e2e6563d69
“John <puxi19@gmail.com>
2023-02-22 13:30:25 +0200

    Fixed Formatting

--- src/components/components/ReinforcementStatement/ReinforcementStatement.js
+++ src/components/components/ReinforcementStatement/ReinforcementStatement.js
@@ -1,27 +1,31 @@
 import React from "react";
 import "./ReinforcementStatement.scss";
-import ReinforcementImage from "./Assets/reinforcementImage.jpg";
+import ReinforcementImage from "./assets/reinforcementImage.jpg";
+
 
 function ReinforcementStatement() {
   return (
-  <div className="Reinforcement">
-            <div className="Reinforcement__image">
-                <img src='./reinforcementImage.jpg' alt=' Reinforcement Image'/>
-            </div>
-            <div className='Reinforcement__statement'>
-                <h2 className='Reinforcement__header'>
-                    Unleash your potential with SkillChain
-                </h2>
-                <u className='Reinforcement__underline'>
-                </u>
-                <p className='Reinforcement__text'>
-                    SkillChain is a modern job search platform that connects job seekers with top employers to find the perfect match for job vacancies. 
-                    It uses the latest technologies, including AI, to offer a personalized and efficient job search experience. SkillChain also provides
-                    continuous upskilling opportunities to help job seekers advance their careers and remain competitive in the job market. With SkillChain, 
-                    job seekers can expect a smarter, faster, and more effective way to find their next career opportunity.           
-                </p>
-            </div>
-         </div>    
+    <div className="Reinforcement">
+      <div className="Reinforcement__image">
+        <img src={ReinforcementImage} alt=" Reinforcement Image" />
+      </div>
+      <div className="Reinforcement__statement">
+        <h2 className="Reinforcement__header">
+          Unleash your potential with SkillChain
+        </h2>
+        <u className="Reinforcement__underline"></u>
+        <p className="Reinforcement__text">
+          SkillChain is a modern job search platform that connects job seekers
+          with top employers to find the perfect match for job vacancies. It
+          uses the latest technologies, including AI, to offer a personalized
+          and efficient job search experience. SkillChain also provides
+          continuous upskilling opportunities to help job seekers advance their
+          careers and remain competitive in the job market. With SkillChain, job
+          seekers can expect a smarter, faster, and more effective way to find
+          their next career opportunity.
+        </p>
+      </div>
+    </div>
   );
 }
 
