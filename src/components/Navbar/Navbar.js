e1b0d238b5b667ae359083ffdb6d1823b315653e
Aiyanna1 <121715056+Aiyanna1@users.noreply.github.com>
2023-02-22 07:07:12 +0200

    Desktop Navbar, updated scss

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,22 +5,14 @@ import "./Navbar.scss"
 function Navbar() {
   return (
     <div className="navbar">
-        <div className="navbar__icon"></div>
-        <div className="navbar__items">
-            <a href="#" className="navbar__link">
-                Find Candidates
-            </a>
-            <a href="#" className="navbar__link">
-                Find Jobs
-            </a>
-            <a href="#" className="navbar__link">
-                Contact Us
-            </a>
-            <a href="#" target="_blank">
-                <button className="navbar__btn">
-                    Sign In
-                </button>
-            </a>
+         <div className="navbar__left">
+            <div className="navbar_icon"><img alt="Logo" src="../../Assets/SkillChain.svg"/></div>
+        </div>
+        <div className="navbar__right">
+            <div className="navbar__links"><a href="#">Find Candidates</a></div>
+            <div className="navbar__links" ><a href="#">Find Jobs</a></div>
+            <div className="navbar__links"><a href="#">Contact Us</a></div>
+            <button className="navbar__btn" target="__blank">Sign In</button>
         </div>
     </div>
   )
