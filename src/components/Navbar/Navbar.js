1ebbfd6aa7ffa7ee8e2fd3f39535fc6eecb3f69f
Aiyanna1 <121715056+Aiyanna1@users.noreply.github.com>
2023-02-22 13:49:08 +0200

    changes to scss and classnames

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,14 +5,14 @@ import "./Navbar.scss"
 function Navbar() {
   return (
     <div className="Navbar">
-         <div className="navbar__left">
-            <div className="navbar__icon"><img alt="Logo" src="assets/skillChain.svg"/></div>
+        <div className="Navbar__left">
+            <div className="Navbar__icon"><img alt="Logo" src="./../../assets/skillChain.svg"/></div>
         </div>
-        <div className="navbar__right">
-            <div className="navbar__links"><a href="#">Find Candidates</a></div>
-            <div className="navbar__links"><a href="#">Find Jobs</a></div>
-            <div className="navbar__links"><a href="#">Contact Us</a></div>
-            <button className="navbar__btn" target="__blank">Sign In</button>
+        <div className="Navbar__right">
+            <div className="Navbar__links"><a href="#">Find Candidates</a></div>
+            <div className="Navbar__links"><a href="#">Find Jobs</a></div>
+            <div className="Navbar__links"><a href="#">Contact Us</a></div>
+            <button className="Navbar__btn" target="__blank">Sign In</button>
         </div>
     </div>
   );
