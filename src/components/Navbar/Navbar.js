7db3c60a916e9a0adb877e121a66f3f05adf37d5
Aiyanna1 <121715056+Aiyanna1@users.noreply.github.com>
2023-02-22 10:24:32 +0200

    changed classname and image path

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,18 +4,18 @@ import "./Navbar.scss"
 
 function Navbar() {
   return (
-    <div className="navbar">
+    <div className="Navbar">
          <div className="navbar__left">
-            <div className="navbar_icon"><img alt="Logo" src="../../Assets/SkillChain.svg"/></div>
+            <div className="navbar__icon"><img alt="Logo" src="assets/skillChain.svg"/></div>
         </div>
         <div className="navbar__right">
             <div className="navbar__links"><a href="#">Find Candidates</a></div>
-            <div className="navbar__links" ><a href="#">Find Jobs</a></div>
+            <div className="navbar__links"><a href="#">Find Jobs</a></div>
             <div className="navbar__links"><a href="#">Contact Us</a></div>
             <button className="navbar__btn" target="__blank">Sign In</button>
         </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
