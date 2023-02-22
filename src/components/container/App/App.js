855e178df4cc3ab50ab44475cc22011293032b7a
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-02-22 12:08:17 +0100

    reverted App.js

--- src/components/container/App/App.js
+++ src/components/container/App/App.js
@@ -1,5 +1,5 @@
-import HeroSection from "../../components/HeroSection/HeroSection";
-import "./App.scss";
+import HeroSection from '../../components/HeroSection/HeroSection';
+import './App.scss';
 
 function App() {
   return (
