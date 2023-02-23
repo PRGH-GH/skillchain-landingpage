c623125d422a4fa2306296078b9b7f6b7614f685
Tim <github@mailtim.de>
2023-02-23 09:01:36 +0100

    Revert App.js

--- src/components/container/App/App.js
+++ src/components/container/App/App.js
@@ -1,12 +1,10 @@
 import HeroSection from '../../components/HeroSection/HeroSection';
-import FeaturedJobs from '../FeaturedJobs/FeaturedJobs';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <HeroSection />
-      <FeaturedJobs />
     </div>
   );
 }
