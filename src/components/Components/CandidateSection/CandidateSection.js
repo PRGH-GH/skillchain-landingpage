d536341673a3f843e59e0b6f0ed1224ab4d89cec
Raveen Liyanage <raveenlng@gmail.com>
2023-02-23 20:38:30 +0530

    fixed positioning of view more button

--- src/components/Components/CandidateSection/CandidateSection.js
+++ src/components/Components/CandidateSection/CandidateSection.js
@@ -6,23 +6,25 @@ function CandidateSection() {
   return (
     <div className='CandidateSection'>
         <div className='CandidateSection__wrapper'>
-            <h2 className='CandidateSection__wrapper__title'>
-                Meet Candidates
-                <div className='CandidateSection__wrapper__title__rec'></div>
-            </h2>
-            <div className='CandidateSection__wrapper__viewmore CandidateSection__wrapper__viewmore--top'>
-              <a href='#' className='CandidateSection__wrapper__viewmore__text'>
-                  View More {" "}
-                  <img className='CandidateSection__wrapper__viewmore__icon' src={ViewMoreIcon} />
-              </a>
-            </div>
-            {/** Insert cards here */}
-            <div className='CandidateSection__wrapper__viewmore CandidateSection__wrapper__viewmore--bottom'>
-              <a href='#' className='CandidateSection__wrapper__viewmore__text'>
-                  View More {" "}
-                  <img className='CandidateSection__wrapper__viewmore__icon' src={ViewMoreIcon} />
-              </a>
-            </div>
+          <div className='CandidateSection__wrapper__title'>
+            <h2 className='CandidateSection__wrapper__title__text'>
+                  Meet Candidates
+                  <div className='CandidateSection__wrapper__title__text__rec'></div>
+              </h2>
+              <div className='CandidateSection__wrapper__title__viewmore CandidateSection__wrapper__title__viewmore--top'>
+                <a href='#' className='CandidateSection__wrapper__viewmore__text'>
+                    View More {" "} 
+                    <img className='CandidateSection__wrapper__viewmore__icon' src={ViewMoreIcon} />
+                </a>
+              </div>
+              {/** Insert cards here */}
+              <div className='CandidateSection__wrapper__title__viewmore CandidateSection__wrapper__title__viewmore--bottom'>
+                <a href='#' className='CandidateSection__wrapper__viewmore__text'>
+                    View More {" "} 
+                    <img className='CandidateSection__wrapper__viewmore__icon' src={ViewMoreIcon} />
+                </a>
+              </div>
+          </div>
         </div>
     </div>
   )
