e155100d54ec436ad5498400e601e5fe7403c415
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-02-23 10:15:27 +0100

    merged main into 21161053

--- src/components/container/App/App.js
+++ src/components/container/App/App.js
@@ -1,14 +1,14 @@
-import HeroSection from '../../components/HeroSection/HeroSection';
-import FeaturedJobs from '../FeaturedJobs/FeaturedJobs';
-import PartnerBanner from '../PartnerBanner/PartnerBanner';
-import './App.scss';
+import HeroSection from "../../components/HeroSection/HeroSection";
+import FeaturedJobs from "../FeaturedJobs/FeaturedJobs";
+import PartnerBanner from "../PartnerBanner/PartnerBanner";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <HeroSection />
-      <FeaturedJobs />
       <PartnerBanner />
+      <FeaturedJobs />
     </div>
   );
 }
