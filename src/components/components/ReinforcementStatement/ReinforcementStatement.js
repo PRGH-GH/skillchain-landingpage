763ec9caff21028ecf01f6bb5ccd43b79ccde397
“John <puxi19@gmail.com>
2023-02-24 12:24:18 +0200

    Fixed Formatting and Media Views

--- src/components/components/ReinforcementStatement/ReinforcementStatement.js
+++ src/components/components/ReinforcementStatement/ReinforcementStatement.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./ReinforcementStatement.scss";
-import ReinforcementImage from "./assets/reinforcementImage.jpg";
+import reinforcementImage from "./assets/reinforcementImage.jpg";
 
 
 function ReinforcementStatement() {
   return (
     <div className="Reinforcement">
       <div className="Reinforcement__image">
-        <img src={ReinforcementImage} alt="Reinforcement Image" />
+        <img src={reinforcementImage} alt="reinforcementImage"/>
       </div>
       <div className="Reinforcement__statement">
         <h2 className="Reinforcement__statement__header">
           Unleash your potential with SkillChain
         </h2>
-        <u className="Reinforcement__statement__underline"></u>
+        <u className="Reinforcement__statement__underline"/>
         <p className="Reinforcement__statement__text">
           SkillChain is a modern job search platform that connects job seekers
           with top employers to find the perfect match for job vacancies. It
