74dc7b969a10d8f7676229b254fc7e2d338c45e2
Anna Lepak <anna.lepak@adesso.de>
2023-02-24 13:34:59 +0100

    added ReasonsSection in App.js

--- src/components/container/App/App.js
+++ src/components/container/App/App.js
@@ -2,6 +2,7 @@ import HeroSection from "../../components/HeroSection/HeroSection";
 import Navbar from "../../components/Navbar/Navbar";
 import FeaturedJobs from "../FeaturedJobs/FeaturedJobs";
 import PartnerBanner from "../PartnerBanner/PartnerBanner";
+import ReasonsSection from "../ReasonsSection/ReasonsSection";
 import Footer from "../../components/Footer/Footer";
 import ReinforcementSection from "../../components/ReinforcementStatement/ReinforcementStatement";
 import "./App.scss";
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <HeroSection />
       <PartnerBanner />
+      <ReasonsSection />
       <ReinforcementSection />
       <FeaturedJobs />
       <Footer />
